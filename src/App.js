36b9703df0ee746335214905c726e741cf4eae91
gg-dev-05 <garvitgalgat@gmail.com>
2021-03-24 13:39:36 +0530

    seconds display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import Digit from './Digit/Digit'
 import './App.scss'
+import { useEffect, useState } from 'react';
 function App() {
   
+  const [secs_0, setSecs_0] = useState(0);
+  const [secs_1, setSecs_1] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const time = new Date();
+      const seconds = time.getSeconds();
+      setSecs_0(parseInt(seconds/10));
+      setSecs_1(parseInt(seconds%10))
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   return (
     <div className="App">
-      <Digit digit={8}/>
-      <Digit digit={9}/>
+      <Digit digit={secs_0}/>
+      <Digit digit={secs_1}/>
       
     </div>
   );
