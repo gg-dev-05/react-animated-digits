7b1fea18333d3c015fcdc6feee858c63baf7985e
gg-dev-05 <garvitgalgat@gmail.com>
2021-03-28 14:21:49 +0530

    body component

--- src/Body.js
+++ src/Body.js
@@ -4,7 +4,7 @@ import App from './App'
 const Body = () => {
     return (
         <div className="body">
-            <h1>HELLO WOLRD</h1>
+            <h1>REACT ANIMATED DIGITS COMPONENT</h1>
             <App/>
         </div>
     )
